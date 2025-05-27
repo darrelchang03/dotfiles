f880135bbf6a094a7997b623b6e6f8206b94006b
Darrel Chang <darrelchang@gmail.com>
2025-05-27 15:21:22 -0700

    added line numbers to netrw

--- nvim/.config/nvim/lua/dchang/set.lua
+++ nvim/.config/nvim/lua/dchang/set.lua
@@ -13,6 +13,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.opt.smartindent = true
+vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
 
 vim.opt.wrap = false
 
