ca6881f78ba78069ac811e3e1c1f7df38d84122e
Darrel Chang <darrelchang@gmail.com>
2026-04-28 11:45:56 -0700

    Removed nvim-treesitter pairs

--- nvim/.config/nvim/lua/dchang/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/dchang/lazy/treesitter.lua
@@ -1,31 +1,32 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all"
-                ensure_installed = {
-                    "vimdoc", "javascript", "typescript", "c", "lua", "rust", "java",
-                    "jsdoc", "bash", "html", "tsx", "css", "json", "yaml", "toml",
-                    "cpp", "python", "go", "query", "regex", "comment"
-                },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    -- A list of parser names, or "all"
+                    ensure_installed = {
+                        "vimdoc", "javascript", "typescript", "c", "lua", "rust", "java",
+                        "jsdoc", "bash", "html", "tsx", "css", "json", "yaml", "toml",
+                        "cpp", "python", "go", "query", "regex", "comment"
+                    },
 
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
+                    -- Install parsers synchronously (only applied to `ensure_installed`)
+                    sync_install = false,
 
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-                auto_install = false,
+                    -- Automatically install missing parsers when entering buffer
+                    -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+                    auto_install = false,
 
-                indent = {
-                    enable = true
-                },
+                    indent = {
+                        enable = true
+                    },
 
-                highlight = {
-                    -- `false` will disable the whole extension
-                    enable = true,
-                    --[[
+                    highlight = {
+                        -- `false` will disable the whole extension
+                        enable = true,
+                        --[[
                     disable = function(lang, buf)
                         if lang == "html" then
                             print("disabled")
@@ -45,135 +46,117 @@ return {
                     end,
                     ]] --
 
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = { "markdown" },
-                },
-                textobjects = {
-                    move = {
-                        enable = true,
-                        set_jumps = false, -- you can change this if you want.
-                        goto_next_start = {
-                            --- ... other keymaps
-                            ["]b"] = { query = "@code_cell.inner", desc = "next code block" },
-                        },
-                        goto_previous_start = {
-                            --- ... other keymaps
-                            ["[b"] = { query = "@code_cell.inner", desc = "previous code block" },
-                        },
+                        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                        -- Instead of true it can also be a list of languages
+                        additional_vim_regex_highlighting = { "markdown" },
                     },
-                    select = {
-                        enable = true,
-                        lookahead = true, -- you can change this if you want
-                        keymaps = {
-                            --- ... other keymaps
-                            ["ib"] = { query = "@code_cell.inner", desc = "in block" },
-                            ["ab"] = { query = "@code_cell.outer", desc = "around block" },
+                    textobjects = {
+                        move = {
+                            enable = true,
+                            set_jumps = false, -- you can change this if you want.
+                            goto_next_start = {
+                                --- ... other keymaps
+                                ["]b"] = { query = "@code_cell.inner", desc = "next code block" },
+                            },
+                            goto_previous_start = {
+                                --- ... other keymaps
+                                ["[b"] = { query = "@code_cell.inner", desc = "previous code block" },
+                            },
                         },
-                    },
-                    swap = { -- Swap only works with code blocks that are under the same
-                        -- markdown header
-                        enable = true,
-                        swap_next = {
-                            --- ... other keymap
-                            ["<leader>sbl"] = "@code_cell.outer",
+                        select = {
+                            enable = true,
+                            lookahead = true, -- you can change this if you want
+                            keymaps = {
+                                --- ... other keymaps
+                                ["ib"] = { query = "@code_cell.inner", desc = "in block" },
+                                ["ab"] = { query = "@code_cell.outer", desc = "around block" },
+                            },
                         },
-                        swap_previous = {
-                            --- ... other keymap
-                            ["<leader>sbh"] = "@code_cell.outer",
+                        swap = { -- Swap only works with code blocks that are under the same
+                            -- markdown header
+                            enable = true,
+                            swap_next = {
+                                --- ... other keymap
+                                ["<leader>sbl"] = "@code_cell.outer",
+                            },
+                            swap_previous = {
+                                --- ... other keymap
+                                ["<leader>sbh"] = "@code_cell.outer",
+                            },
                         },
+                    }
+                })
+
+                local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+                treesitter_parser_config.templ = {
+                    install_info = {
+                        url = "https://github.com/vrischmann/tree-sitter-templ.git",
+                        files = { "src/parser.c", "src/scanner.c" },
+                        branch = "master",
                     },
                 }
-            })
 
-            local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-            treesitter_parser_config.templ = {
-                install_info = {
-                    url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                    files = { "src/parser.c", "src/scanner.c" },
-                    branch = "master",
-                },
-            }
+                vim.treesitter.language.register("templ", "templ")
+            end
+        },
 
-            vim.treesitter.language.register("templ", "templ")
-        end
-    },
-
-    {
-        "theHamsta/nvim-treesitter-pairs",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                pairs = {
-                    enable = true,
-                    highlight_pair_events = { 'CursorMoved' }, -- Highlight pairs on cursor move
-                    highlight_self = true,                     -- Highlight the opening/closing pair under the cursor
-                },
-            })
-        end
-    },
-    {
-        'windwp/nvim-ts-autotag',
-        config = function()
-            require('nvim-ts-autotag').setup({
-                -- Optional configurations can be added here
-            })
-        end,
-    },
-    {
-        'norcalli/nvim-colorizer.lua',
-        config = function()
-            require('colorizer').setup()
-        end
-    },
-    {
-        'windwp/nvim-autopairs',
-        enabled = false,
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
-    },
-    {
-        "SmiteshP/nvim-navic",
-        dependencies = "neovim/nvim-lspconfig", -- Ensure LSP is available
-        config = function()
-            require("nvim-navic").setup({
-                -- Optional: Customize icons or settings
-                icons = {
-                    File = "󰈙 ",
-                    Module = " ",
-                    Namespace = "󰌗 ",
-                    Package = "󰏖 ",
-                    Class = "󰌗 ",
-                    Method = "󰆧 ",
-                    Property = " ",
-                    Field = "󰜢 ",
-                    Constructor = "󰆧 ",
-                    Enum = "󰕘 ",
-                    Interface = "󰜢 ",
-                    Function = "󰊕 ",
-                    Variable = "󰀫 ",
-                    Constant = "󰏿 ",
-                    String = "󰀬 ",
-                    Number = "󰎠 ",
-                    Boolean = "󰨙 ",
-                    Array = "󰅪 ",
-                    Object = "󰀬 ",
-                    Key = "󰌋 ",
-                    Null = "󰟢 ",
-                    EnumMember = " ",
-                    Struct = "󰌗 ",
-                    Event = " ",
-                    Operator = "󰆕 ",
-                    TypeParameter = "󰊄 ",
-                },
-                highlight = true,
-                separator = " > ",
-                depth_limit = 0,
-                depth_limit_indicator = "..",
-            })
-        end,
-    },
+        -- auto html tag
+        {
+            'windwp/nvim-ts-autotag',
+            config = function()
+                require('nvim-ts-autotag').setup({
+                    -- Optional configurations can be added here
+                })
+            end,
+        },
+        {
+            'norcalli/nvim-colorizer.lua',
+            config = function()
+                require('colorizer').setup()
+            end
+        },
+        {
+            "SmiteshP/nvim-navic",
+            dependencies = "neovim/nvim-lspconfig", -- Ensure LSP is available
+            config = function()
+                require("nvim-navic").setup({
+                    -- Optional: Customize icons or settings
+                    icons = {
+                        File = "󰈙 ",
+                        Module = " ",
+                        Namespace = "󰌗 ",
+                        Package = "󰏖 ",
+                        Class = "󰌗 ",
+                        Method = "󰆧 ",
+                        Property = " ",
+                        Field = "󰜢 ",
+                        Constructor = "󰆧 ",
+                        Enum = "󰕘 ",
+                        Interface = "󰜢 ",
+                        Function = "󰊕 ",
+                        Variable = "󰀫 ",
+                        Constant = "󰏿 ",
+                        String = "󰀬 ",
+                        Number = "󰎠 ",
+                        Boolean = "󰨙 ",
+                        Array = "󰅪 ",
+                        Object = "󰀬 ",
+                        Key = "󰌋 ",
+                        Null = "󰟢 ",
+                        EnumMember = " ",
+                        Struct = "󰌗 ",
+                        Event = " ",
+                        Operator = "󰆕 ",
+                        TypeParameter = "󰊄 ",
+                    },
+                    highlight = true,
+                    separator = " > ",
+                    depth_limit = 0,
+                    depth_limit_indicator = "..",
+                })
+            end,
+        },
+    }
 }
