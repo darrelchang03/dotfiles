89912b569748553dd889330cbe1893fbc2b9a071
Darrel Chang <darrelchang@gmail.com>
2026-05-26 21:14:47 -0700

    Made fugitive keymaps show in vim keymap help

--- nvim/.config/nvim/lua/dchang/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/dchang/lazy/fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[Fugitive]: Open Fugitive"})
 
         local dchang_Fugitive = vim.api.nvim_create_augroup("dchang_Fugitive", {})
 
@@ -17,21 +17,21 @@ return {
                 local bufnr = vim.api.nvim_get_current_buf()
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
-                end, {buffer = bufnr, remap = false, desc = '[Fugitive]: Push to upstream'})
+                end, {buffer = bufnr, remap = false, desc = "[Fugitive]: Push to upstream"})
 
                 -- rebase always
                 vim.keymap.set("n", "<leader>P", function()
                     vim.cmd.Git({'pull',  '--rebase'})
-                end, {buffer = bufnr, remap = false, desc = '[Fugitive]: Pull with rebase'})
+                end, {buffer = bufnr, remap = false, desc = "[Fugitive]: Pull with rebase"})
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", {buffer = bufnr, remap = false, desc = '[Fugitive]: Push to branch'});
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", {buffer = bufnr, remap = false, desc = "[Fugitive]: Push to branch" });
             end,
         })
 
 
-        vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = '[Fugitive]: Pick left diff' })
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", { desc = '[Fugitive]: Pick right diff' })
+        vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = "[Fugitive]: Pick left diff" })
+        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", { desc = "[Fugitive]: Pick right diff" })
     end
 }
