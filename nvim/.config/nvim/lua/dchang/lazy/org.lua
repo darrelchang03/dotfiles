146662b27c17882bd7078ccb85d83d4fe21ddbc0
Darrel Chang <darrelchang@gmail.com>
2026-08-10 17:34:58 -0700

    Added org roam mode

--- nvim/.config/nvim/lua/dchang/lazy/org.lua
+++ nvim/.config/nvim/lua/dchang/lazy/org.lua
@@ -1,16 +1,41 @@
-return
-{
-    'nvim-orgmode/orgmode',
-    event = 'VeryLazy',
-    config = function()
-        -- Setup orgmode
-        local org = require('orgmode')
+return {
+    -- Base Org Mode Plugin
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        config = function()
+            -- Setup orgmode
+            local org = require('orgmode')
 
-        org.setup({
-            org_agenda_files = '~/orgfiles/**/*',
-            org_default_notes_file = '~/orgfiles/refile.org',
-        })
-        -- Experimental LSP support
-        vim.lsp.enable('org')
-    end,
+            org.setup({
+                org_agenda_files = '~/org/**/*',
+                org_default_notes_file = '~/org/refile.org',
+            })
+            -- Experimental LSP support
+            vim.lsp.enable('org')
+        end,
+    },
+    -- Org Roam
+    {
+        "chipsenkbeil/org-roam.nvim",
+        tag = "0.2.0",
+        dependencies = {
+            {
+                "nvim-orgmode/orgmode",
+                tag = "0.7.0",
+            },
+        },
+        config = function()
+            require("org-roam").setup({
+                directory = "~/org",
+                -- optional
+                org_files = {
+                    -- "~/another_org_dir",
+                    -- "~/some/folder/*.org",
+                    -- "~/a/single/org_file.org",
+                }
+            })
+        end
+    }
+
 }
