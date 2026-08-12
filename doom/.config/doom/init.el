9242e67bac99c6ed0ceccc4fd20c5f46703a0dd1
Darrel Chang <darrelchang@gmail.com>
2026-08-12 14:27:05 -0700

    Added org roam and set roam directory

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -157,7 +157,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        ;;odin              ; C, minus its footguns
-       (org +lsp +tree-sitter)               ; organize your plain life in plain text
+       (org +roam)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
